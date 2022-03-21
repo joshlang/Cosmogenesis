54addac06cc9b5e79e38a261e4d3212236077be4
Josh <josh@joshlang.ca>
2022-03-20 20:28:19 -0700

    Cleanup

--- Cosmogenesis.Core/DbModelFactory.cs
+++ Cosmogenesis.Core/DbModelFactory.cs
@@ -6,23 +6,12 @@ public static class DbModelFactory
 {
     public static BatchResult CreateBatchResult(DbConflictType conflict) => new(conflict: conflict);
     public static BatchResult CreateBatchResult() => new(0);
-
-    public static CreateResult<T> CreateCreateResult<T>(DbConflictType conflict) where T : DbDoc =>
-        new(conflict: conflict);
-    public static CreateResult<T> CreateCreateResult<T>(T document) where T : DbDoc =>
-        new(document: document);
-
-    public static ReplaceResult<T> CreateReplaceResult<T>(DbConflictType conflict) where T : DbDoc =>
-        new(conflict: conflict);
-    public static ReplaceResult<T> CreateReplaceResult<T>(T document) where T : DbDoc =>
-        new(document: document);
-
-    public static CreateOrReplaceResult<T> CreateCreateOrReplaceResult<T>(T document, bool alreadyExisted) where T : DbDoc =>
-        new(document: document, alreadyExisted: alreadyExisted);
-
-    public static ReadOrCreateResult<T> CreateReadOrCreateResult<T>(T document, bool alreadyExisted) where T : DbDoc =>
-        new(document: document, alreadyExisted: alreadyExisted);
-
+    public static CreateResult<T> CreateCreateResult<T>(DbConflictType conflict) where T : DbDoc => new(conflict: conflict);
+    public static CreateResult<T> CreateCreateResult<T>(T document) where T : DbDoc => new(document: document);
+    public static ReplaceResult<T> CreateReplaceResult<T>(DbConflictType conflict) where T : DbDoc => new(conflict: conflict);
+    public static ReplaceResult<T> CreateReplaceResult<T>(T document) where T : DbDoc => new(document: document);
+    public static CreateOrReplaceResult<T> CreateCreateOrReplaceResult<T>(T document, bool alreadyExisted) where T : DbDoc => new(document: document, alreadyExisted: alreadyExisted);
+    public static ReadOrCreateResult<T> CreateReadOrCreateResult<T>(T document, bool alreadyExisted) where T : DbDoc => new(document: document, alreadyExisted: alreadyExisted);
     public static DbUnknownStatusCodeException CreateDbUnknownStatusCodeException(HttpStatusCode statusCode) => new(statusCode: statusCode);
     public static DbOverloadedException CreateDbOverloadedException() => new();
     public static DbOverloadedException CreateDbOverloadedException(string message) => new(message: message);
